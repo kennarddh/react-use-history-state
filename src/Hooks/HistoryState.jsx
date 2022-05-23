a00d2537b17848f1f2de51f39022c5fe856c06fa
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-23 15:08:04 +0700

    feat: Change state return boolean

--- src/Hooks/HistoryState.jsx
+++ src/Hooks/HistoryState.jsx
@@ -6,13 +6,15 @@ const useHistoryState = initialState => {
 	const [Pointer, SetPointer] = useState(0)
 
 	const ChangeState = value => {
-		if (value === History.at(-1)) return
+		if (value === History.at(-1)) return false
 
 		SetState(value)
 
 		SetHistory(history => [...history, value])
 
 		SetPointer(pointer => pointer + 1)
+
+		return true
 	}
 
 	const Undo = () => {
