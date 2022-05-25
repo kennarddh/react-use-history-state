716a36549f1e5ead5af7da285dd6e78890948618
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-05-25 12:05:46 +0700

    perf: Use callback

--- src/Hooks/HistoryState.jsx
+++ src/Hooks/HistoryState.jsx
@@ -1,27 +1,30 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 const useHistoryState = initialState => {
 	const [State, SetState] = useState(initialState)
 	const [History, SetHistory] = useState([initialState])
 	const [Pointer, SetPointer] = useState(0)
 
-	const ChangeState = value => {
-		if (value === History[History.length - 1]) return false
+	const ChangeState = useCallback(
+		value => {
+			if (value === History[History.length - 1]) return false
 
-		SetState(value)
+			SetState(value)
 
-		SetHistory(history => {
-			if (Pointer === history.length - 1) return [...history, value]
+			SetHistory(history => {
+				if (Pointer === history.length - 1) return [...history, value]
 
-			return [...history.slice(0, Pointer + 1), value]
-		})
+				return [...history.slice(0, Pointer + 1), value]
+			})
 
-		SetPointer(pointer => pointer + 1)
+			SetPointer(pointer => pointer + 1)
 
-		return true
-	}
+			return true
+		},
+		[Pointer, History]
+	)
 
-	const Undo = () => {
+	const Undo = useCallback(() => {
 		if (Pointer === 0) return false
 
 		SetState(History[Pointer - 1])
@@ -29,9 +32,9 @@ const useHistoryState = initialState => {
 		SetPointer(pointer => pointer - 1)
 
 		return true
-	}
+	}, [History, Pointer])
 
-	const Redo = () => {
+	const Redo = useCallback(() => {
 		if (Pointer === History.length - 1) return false
 
 		SetState(History[Pointer + 1])
@@ -39,7 +42,7 @@ const useHistoryState = initialState => {
 		SetPointer(pointer => pointer + 1)
 
 		return true
-	}
+	}, [History, Pointer])
 
 	return [State, ChangeState, [Undo, Redo, History]]
 }
